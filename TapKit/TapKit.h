f78ac11e90476785ec7dc2d9c6c720825602b58a
Rob Caraway <rcaraway@inmotionsoftware.com>
2019-06-11 17:52:05 -0500

    Initial Commit

---
+++ TapKit/TapKit.h
@@ -0,0 +1,19 @@
+//
+//  TapKit.h
+//  TapKit
+//
+//  Created by Rob Caraway on 6/11/19.
+//  Copyright © 2019 Rob Caraway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TapKit.
+FOUNDATION_EXPORT double TapKitVersionNumber;
+
+//! Project version string for TapKit.
+FOUNDATION_EXPORT const unsigned char TapKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapKit/PublicHeader.h>
+
+
